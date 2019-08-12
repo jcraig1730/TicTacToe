e326fbc9aece46fced27d48baf7e478a1dc3c03d
jared <jcraig1730@gmail.com>
2019-08-12 16:30:41 -0700

    Add basic html, styling and functionality for tic tac toe.

---
+++ app.js
@@ -0,0 +1,236 @@
+const gameBoard = document.querySelector("#game-board");
+const topLeft = document.querySelector("#top-row .left");
+const topMiddle = document.querySelector("#top-row .middle");
+const topRight = document.querySelector("#top-row .right");
+const middleLeft = document.querySelector("#middle-row .left");
+const middleMiddle = document.querySelector("#middle-row .middle");
+const middleRight = document.querySelector("#middle-row .right");
+const bottomLeft = document.querySelector("#bottom-row .left");
+const bottomMiddle = document.querySelector("#bottom-row .middle");
+const bottomRight = document.querySelector("#bottom-row .right");
+const turnText = document.querySelector(".turn");
+const msg = document.querySelector("#msg");
+const reset = document.querySelector(".reset");
+const newGame = document.querySelector(".new-game");
+const player1Score = document.querySelector(".player-1-score");
+const player2Score = document.querySelector(".player-2-score");
+const player1NameSubmit = document.querySelector(".player-1-name-submit");
+const player2NameSubmit = document.querySelector(".player-2-name-submit");
+
+let gameState = {
+  player: null,
+  board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+  p1Score: 0,
+  p2Score: 0,
+  player1Name: null,
+  player2Name: null
+};
+
+const nextTurn = () => {
+  if (gameState.player === "Player 1") {
+    gameState.player = "Player 2";
+  } else {
+    gameState.player = "Player 1";
+  }
+  if (gameState.player === "Player 1") {
+    console.log("hi");
+    turnText.innerText = `${gameState.player1Name || gameState.player}'s turn`;
+  } else {
+    console.log("p2");
+    turnText.innerText = `${gameState.player2Name || gameState.player}'s turn`;
+  }
+};
+
+const checkWinner = () => {
+  // check for one player across an entire row
+  gameState.board.forEach(row => {
+    if (row[0] !== 0 && row[0] === row[1] && row[1] === row[2]) {
+      msg.innerText = `Player ${row[0]} wins!`;
+      if (row[0] === 1) {
+        gameState.p1Score++;
+      } else {
+        gameState.p2Score++;
+      }
+      player1Score.innerText = `${gameState.player1Name || "Player 1"}: ${
+        gameState.p1Score
+      }`;
+      player2Score.innerText = `${gameState.player2Name || "Player 2"}: ${
+        gameState.p2Score
+      }`;
+      gameBoard.removeEventListener("click", handleClick, false);
+    }
+  });
+
+  // check for one player across an entire col
+  for (let col = 0; col < 3; col++) {
+    if (
+      gameState.board[0][col] !== 0 &&
+      gameState.board[0][col] === gameState.board[1][col] &&
+      gameState.board[1][col] === gameState.board[2][col]
+    ) {
+      msg.innerText = `Player ${gameState.board[0][col]} wins!`;
+      if (gameState.board[0][col] === 1) {
+        gameState.p1Score++;
+      } else {
+        gameState.p2Score++;
+      }
+      player1Score.innerText = `${gameState.player1Name || "Player 1"}: ${
+        gameState.p1Score
+      }`;
+      player2Score.innerText = `${gameState.player2Name || "Player 2"}: ${
+        gameState.p2Score
+      }`;
+      gameBoard.removeEventListener("click", handleClick, false);
+    }
+  }
+
+  // check diagonals
+  for (let i = 0; i < 3; i++) {
+    if (
+      gameState.board[0][0] !== 0 &&
+      gameState.board[0][0] === gameState.board[1][1] &&
+      gameState.board[1][1] === gameState.board[2][2]
+    ) {
+      msg.innerText = `Player ${gameState.board[0][0]} wins!`;
+      if (gameState.board[1][1] === 1) {
+        console.log("hi");
+        gameState.p1Score++;
+      } else {
+        console.log("hi");
+        gameState.p2Score++;
+      }
+      player1Score.innerText = `${gameState.player1Name || "Player 1"}: ${
+        gameState.p1Score
+      }`;
+      player1Score.innerText = `${gameState.player2Name || "Player 2"}: ${
+        gameState.p2Score
+      }`;
+
+      gameBoard.removeEventListener("click", handleClick, false);
+      break;
+    } else if (
+      gameState.board[0][2] !== 0 &&
+      gameState.board[0][2] === gameState.board[1][1] &&
+      gameState.board[1][1] === gameState.board[2][0]
+    ) {
+      msg.innerText = `Player ${gameState.board[0][2]} wins!`;
+      if (gameState.board[1][1] === 1) {
+        gameState.p1Score++;
+      } else {
+        gameState.p2Score++;
+      }
+      player1Score.innerText = `${gameState.player1Name || "Player 1"}: ${
+        gameState.p1Score
+      }`;
+      player2Score.innerText = `${gameState.player2Name || "Player 2"}: ${
+        gameState.p2Score
+      }`;
+      gameBoard.removeEventListener("click", handleClick, false);
+      break;
+    }
+  }
+
+  // check for a draw
+  let boardFull = true;
+  gameState.board.forEach(row => {
+    row.forEach(col => {
+      if (col === 0) {
+        boardFull = false;
+      }
+    });
+  });
+  if (boardFull) {
+    msg.innerText = "It's a draw!";
+  }
+};
+
+const markMatrix = (targetRow, targetCol) => {
+  switch (targetRow) {
+    case "top-row":
+      targetRow = 0;
+      break;
+    case "middle-row":
+      targetRow = 1;
+      break;
+    case "bottom-row":
+      targetRow = 2;
+  }
+  switch (targetCol) {
+    case "left":
+      targetCol = 0;
+      break;
+    case "middle":
+      targetCol = 1;
+      break;
+    case "right":
+      targetCol = 2;
+      break;
+  }
+  if (gameState.player === "Player 1") {
+    gameState.board[targetRow][targetCol] = 1;
+  } else {
+    gameState.board[targetRow][targetCol] = 2;
+  }
+};
+
+const handleClick = e => {
+  let targetRow = e.path[1].id;
+  let targetCol = e.path[0].classList[0];
+  if (!targetRow || !targetCol) {
+    msg.innerText = "Choose a different square";
+  } else {
+    let target = document.querySelector(`#${targetRow} .${targetCol}`);
+    markMatrix(targetRow, targetCol);
+
+    let newElement = document.createElement("div");
+    newElement.className =
+      gameState.player === "Player 1" ? "player1" : "player2";
+    target.append(newElement);
+    msg.innerText = "";
+    checkWinner();
+    nextTurn();
+  }
+};
+
+reset.addEventListener("click", () => {
+  gameState = {
+    player: null,
+    board: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+    p1Score: 0,
+    p2Score: 0
+  };
+  let player1Elements = document.querySelectorAll(".player1");
+  player1Elements.forEach(el => el.remove());
+  let player2Elements = document.querySelectorAll(".player2");
+  player2Elements.forEach(el => el.remove());
+  msg.innerText = "";
+  gameBoard.addEventListener("click", handleClick);
+  player1Score.innerText = `Player 1: ${gameState.p1Score}`;
+  player2Score.innerText = `Player 2: ${gameState.p2Score}`;
+});
+
+newGame.addEventListener("click", () => {
+  gameState.board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+  let player1Elements = document.querySelectorAll(".player1");
+  player1Elements.forEach(el => el.remove());
+  let player2Elements = document.querySelectorAll(".player2");
+  player2Elements.forEach(el => el.remove());
+  msg.innerText = "";
+  gameBoard.addEventListener("click", handleClick);
+});
+
+gameBoard.addEventListener("click", handleClick);
+
+player1NameSubmit.addEventListener("click", () => {
+  let name = document.querySelector(".p1-name").value;
+  gameState.player1Name = name;
+  player1Score.innerText = `${gameState.player1Name}: ${gameState.p1Score}`;
+});
+
+player2NameSubmit.addEventListener("click", () => {
+  let name = document.querySelector(".p2-name").value;
+  gameState.player2Name = name;
+  player2Score.innerText = `${gameState.player2Name}: ${gameState.p2Score}`;
+});
+
+nextTurn();
